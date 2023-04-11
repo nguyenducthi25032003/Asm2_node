a9d50e143f2b0e1421fec484ba76579394484f20
nguyenducthi25032003 <119672912+nguyenducthi25032003@users.noreply.github.com>
2023-04-11 17:53:43 +0700

    Add files via upload

---
+++ src/models/product.js
@@ -0,0 +1,20 @@
+import { string } from "joi";
+import mongoose from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+
+const productSchema = new mongoose.Schema(
+    {
+        name: String,
+        price: Number,
+        description:String,
+        categoryId: {
+            type: mongoose.Types.ObjectId,
+            ref: "Category",
+        },
+    },
+    { timestamps: true, versionKey: false }
+);
+
+productSchema.plugin(mongoosePaginate);
+
+export default mongoose.model("Product", productSchema);
